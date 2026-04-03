237fa3db5af7ea86b04366f0d0dc78b55c0de0b0
GamalOn <garance.boudot@gmail.com>
2026-04-03 09:17:07 +0200

    yip : structure

--- partitionnement/molecular_density/main_molecular_density.py
+++ partitionnement/molecular_density/main_molecular_density.py
@@ -37,54 +37,6 @@ def molecular_orbital_calcul_3d(x, y, z, fichier):
     probility_densities = np.square(molecular_orbital_somme)
     return probility_densities, molecular_orbital_somme
 
-def plot_result_1d(position, density) :
-    """Plot the probility density of each molecular orbital"""
-    plt.figure()
-    plt.grid(True)
-    for i in range(0, len(density)) : 
-        plt.plot(position, density[i], ".", label=f"orbital {i}")
-        plt.xlabel("Position, en Bohr radius")
-        plt.ylabel("Densités électroniques")
-        i+=1 
-    plt.legend()
-    plt.show()
-
-
-def plot_orbital_3d(density, x_range, y_range, z_range, level=None, title="Orbital 3D"):
-    """
-    Plotting the densities in 3D space
-    Arguments : 
-    - density : 
-    - x_range, y_range, z_range : 
-    """
-
-    # Grille 3D
-    X, Y, Z = np.meshgrid(x_range, y_range, z_range, indexing='ij')
-
-    # Valeur de seuil
-    if level is None:
-        level = 0.3 * np.max(np.abs(density))
-
-    fig = go.Figure(data=go.Isosurface(
-        x=X.ravel(),
-        y=Y.ravel(),
-        z=Z.ravel(),
-        value=density.ravel(),
-        isomin=level,
-        isomax=density.max(),
-        surface_count=1,
-        caps=dict(x_show=False, y_show=False, z_show=False),
-    ))
-
-    fig.update_layout(
-        title=title,
-        scene=dict(
-            xaxis_title='x',
-            yaxis_title='y',
-            zaxis_title='z',
-        )
-    )
-    fig.show()
 
 def main():
     parser = argparse.ArgumentParser(description="Calcul des orbitales moléculaires.")
@@ -98,9 +50,7 @@ def main():
     z = np.linspace(-6e-2, 6e-2, 80)
 
     densities, mo = molecular_orbital_calcul_3d(x, y, z, args.filepath)
-    #print(densities)
-    plot_orbital_3d(densities, x, y, x)
-
+    print(densities)
 
 
 if __name__ == "__main__":
