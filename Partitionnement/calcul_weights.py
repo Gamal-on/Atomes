0e628821665af9534dec3e22f679836cc1b0ece0
GamalOn <garance.boudot@gmail.com>
2026-05-15 10:59:00 +0200

    yip : calcul poids

--- Partitionnement/calcul_weights.py
+++ Partitionnement/calcul_weights.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-from read_weights import density_promolecule, density_carbon_1, density_oxygen_2, density_azote_3
-from read_weights import density_hydrogen_4, density_hydrogen_5, density_azote_6, density_hydrogen_7, density_hydrogen_8
-from read_weights import grid
 
 def hirshfeld_weight(density_atom, density_promolecule):
     poids = np.divide(
@@ -12,11 +9,3 @@ def hirshfeld_weight(density_atom, density_promolecule):
     where=density_promolecule != 0,)
 
     return poids
-
-def main() : 
-    print(len(density_promolecule), len(density_carbon_1))
-    weight_carbon = hirshfeld_weight(density_carbon_1, density_promolecule)
-    print(weight_carbon)
-
-if __name__ == "__main__":
-    main()
---
+++ Partitionnement/export_weights.py
@@ -0,0 +1,12 @@
+import numpy as np
+from pathlib import Path
+
+def export_hirshfeld_weights(grid_coordinates, weights, filename, folder_name="Weights_Urea_Grid"):
+
+    data_to_save = np.column_stack((grid_coordinates, weights))
+
+    output_dir = Path(folder_name)
+    file_path = output_dir / filename
+    
+    np.savetxt(file_path, data_to_save)
+
