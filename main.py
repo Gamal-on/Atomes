bb9e19ddf17c64bf50c6bdedd1b593b37aacad94
GamalOn <garance.boudot@gmail.com>
2026-06-08 13:45:38 +0200

    yip : refactoring

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from Extract_Parameters.extract_data import extract_urea_data
+from Grid.becke_grid import generate_grid, plot_multicenter_grid, export_grid_for_multiwfn
+
+
+
 def main():
-    print("Hello from atomes!")
 
-
-if __name__ == "__main__":
-    main()
-
-# Data
-
-# export_multiwfn_data
-
-# Grid
-
-# generate_grid
-# export_grid
+    # Charge data
+    
+    centers, atome_dict = extract_urea_data("./Multiwfn_data/urea.wfn")
+    
+    # Grid, default paramaters
+    radial_lenght = 20
+    ordre_choisi = 53
+    r_m = 0.5
+    
+    grid = generate_grid(radial_lenght, centers, ordre_choisi, r_m=0.5, units='bohr')
+    
+    # Si les 
+    # export_grid(grid, output_path, centers_bohr, r_cutoff=50.0), si jamais, on a déjà le fichier
 # plot_multicenter_grid (optional)
 
 # grid = generate_grid(20, centers, ordre_choisi=53, r_m=0.5, units='bohr')
@@ -24,3 +29,5 @@ if __name__ == "__main__":
 # les fichiers de poids sont dans Data/
 # pour plot le poids d'un atome: main_partitioning
 
+if __name__ == "__main__":
+    main()
