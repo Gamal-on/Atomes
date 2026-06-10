810736d1b3f4bcd1cadf69ce8316f85d252fb001
GamalOn <garance.boudot@gmail.com>
2026-06-10 16:54:19 +0200

    yip : readme

--- Partitioning/plot_weights.py
+++ Partitioning/plot_weights.py
@@ -5,13 +5,14 @@ import pandas as pd
 import plotly.express as px
 
 
-def plot_weight(grid, weight):
-    
+def plot_weight(grid, atom_dict):
+    atom_id = int(input("Numéro d'atome : "))
+
     fig = px.scatter_3d(
     x=grid[:, 0],
     y=grid[:, 1],
     z=grid[:, 2],
-    color=weight,  # La couleur dépend du poids
+    color=atom_dict[atom_id]['hirshfeld_weight'],  # La couleur dépend du poids
     color_continuous_scale="Viridis",  # Échelle de couleur
     labels={"x": "X", "y": "Y", "z": "Z", "color": "Poids"},
     title="Poids de Hirshfeld de l'atome d'oxygène (molécule d'urée)",
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from Extract_Parameters.extract_promolecule_density import load_density_promolecule
 from state import ProjectState
 from Extract_Parameters.extract_urea_data import extract_urea_data
-from Grid.becke_grid import generate_grid
+from Grid.becke_grid import generate_grid, plot_multicenter_grid
 from Extract_Parameters.extract_atomic_densities import load_density
 from Partitioning.calcul_weights import hirshfeld_weight
 from Partitioning.plot_weights import plot_weight
 
 state = ProjectState()
 
-# Parsing the molecular info from urea.wfn
+## --- 1. Parsing the information from Multiwfn files ---
+
+# Molecular (centers, number of atom ...) from urea.wfn
 
 state.centers, state.atom_dict = extract_urea_data("Multiwfn_Data/urea.txt")
-
-# Grid : do not change the parameters, unless you are willing to generate new density files with Multiwfn
-
-state.grid = generate_grid(19, state.centers, 53)
-
 
 # Parsing atomic densities info from Multiwfn_Data/Isolated_Densities
 
@@ -28,22 +25,31 @@ state.atom_dict = load_density("Multiwfn_Data/Isolated_Densities/oxygen_2.txt", 
 state.atom_dict = load_density("Multiwfn_Data/Isolated_Densities/hydrogen_8.txt", state.atom_dict)
 state.atom_dict = load_density("Multiwfn_Data/Isolated_Densities/hydrogen_7.txt", state.atom_dict)
 
-# Parsing promolecule density from Multiwfn_Data/Promolecule_Density/urea_promolecule_density.txt
+# Parsing promolecule density 
 
 state.promolecule_density = load_density_promolecule("Multiwfn_Data/urea_promolecule_density.txt")
 
-# Calculating Hirshfeld weights
+
+## --- 2. Generating the grid ---
+
+# Do not change the parameters, unless you are willing to generate new density files with Multiwfn
+
+state.grid = generate_grid(19, state.centers, 53)
+
+# Plotting the grid : to visualize the grid, enter the following line of code : 
+
+# plot_multicenter_grid(state.grid, state.centers)
+
+
+
+## --- 3. Hirshfeld weights ---
+
+#Calculating weights
 
 state.atom_dict = hirshfeld_weight(state.atom_dict, state.promolecule_density)
 
-# print(state.atom_dict[1]['hirshfeld_weight'])
-# print(state.grid)
-
-# Exporting Hirshfedl weights
-
 # Plotting Hirshfeld weights : for the  atom number x, enter 
 
-print(state.atom_dict)
+grid_flat = state.grid.reshape(-1, 3)
+plot_weight(grid_flat,state.atom_dict)
 
-# grid_flat = state.grid.reshape(-1, 3)
-# plot_weight(grid_flat, state.atom_dict[1]['hirshfeld_weight'])
