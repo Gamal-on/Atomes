49ef8133fab758f9250752b8c79c361922752a7c
GamalOn <garance.boudot@gmail.com>
2026-06-10 11:39:09 +0200

    yip : refactoring grid

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Extract_Parameters.extract_promolecule_density import load_density_promolecule
 from state import ProjectState
 from Extract_Parameters.extract_urea_data import extract_urea_data
 from Grid.becke_grid import generate_grid
@@ -6,11 +7,10 @@ from Partitioning.calcul_weights import hirshfeld_weight
 from Partitioning.plot_weights import plot_weight
 
 state = ProjectState()
-state.centers = extract_urea_data("Multiwfn_Data/urea.wfn")
 
 # Parsing the molecular info from urea.wfn
 
-state.centers, state.atom_dict = extract_urea_data("Multiwfn_Data/urea.wfn")
+state.centers, state.atom_dict = extract_urea_data("Multiwfn_Data/urea.txt")
 
 # Grid 
 
@@ -30,14 +30,18 @@ state.atom_dict = load_density("Multiwfn_Data/Isolated_Densities/hydrogen_7.txt"
 
 # Parsing promolecule density from Multiwfn_Data/Promolecule_Density/urea_promolecule_density.txt
 
-state.promolecule_density = load_density("Multiwfn_Data/urea_promolecule_density.txt", state.atom_dict)
+state.promolecule_density = load_density_promolecule("Multiwfn_Data/urea_promolecule_density.txt")
 
 # Calculating Hirshfeld weights
 
 state.atom_dict = hirshfeld_weight(state.atom_dict, state.promolecule_density)
 
+# print(state.atom_dict[1]['hirshfeld_weight'])
+# print(state.grid)
+
 # Exporting Hirshfedl weights
 
 # Plotting Hirshfeld weights
 
-plot_weight(state.grid, state.atom_dict["O"]["weight"])
+grid_flat = state.grid.reshape(-1, 3)
+plot_weight(grid_flat, state.atom_dict[1]['hirshfeld_weight'])
