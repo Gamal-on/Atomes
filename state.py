396eb5d06cf145c09708df0f9bd1e93caae7d4d0
GamalOn <garance.boudot@gmail.com>
2026-09-24 09:59:49 +0200

    tests  passed

--- state.py
+++ state.py
@@ -6,6 +6,6 @@ import numpy as np
 @dataclass
 class ProjectState:
     centers: np.ndarray = None
-    atom_dict: dict = field(default_factory=dict) #{'element, 'coord', 'rho', 'weight}
+    atom_dict: dict = field(default_factory=dict) #{'element, 'coords', 'rho', 'weight}
     promolecule_density: np.ndarray = None
     grid_points: np.ndarray = None
